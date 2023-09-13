af61313940e276d896d357a411cdd7f6ee15810b
aleguiter09 <aleguiter9@gmail.com>
2023-09-13 13:57:34 -0300

    Navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <nav className="flex justify-center mt-3">
+            <ul className="max-w-screen-xl flex gap-6">
+              <li className="cursor-pointer hover:text-blue-500">Home</li>
+              <li className="cursor-pointer hover:text-blue-500">Experience</li>
+              <li className="cursor-pointer hover:text-blue-500">Projects</li>
+            </ul>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
