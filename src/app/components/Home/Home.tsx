0d29f28f170821558805b2f35d2f01c9476f24c4
aguiter-inditex <alejandrogui@ext.inditex.com>
2024-06-13 16:59:39 +0200

    Fixed build problem

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -1,7 +1,6 @@
-import Education from "../Education/Education";
 import Experience from "../Experience/Experience";
 import Presentation from "../Presentation/Presentation";
-import Projects from "../Projects/Projects";
+// import Projects from "../Projects/Projects";
 import "./Home.scss";
 
 export default function Home() {
