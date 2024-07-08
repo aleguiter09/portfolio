f7da439ba57b26d93d523a007cec2b649f5a848f
aguiter-inditex <alejandrogui@ext.inditex.com>
2024-07-08 13:12:23 +0200

    New images on Project section

--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -1,6 +1,7 @@
 import "./Projects.scss";
 import Image from "next/image";
 import { projects } from "@/constants/projects";
+import Link from "next/link";
 
 export default function Projects() {
   return (
@@ -10,25 +11,49 @@ export default function Projects() {
         {projects.map((project) => {
           return (
             <div className="project_container" key={project.id}>
-              <Image
-                className="project__image"
-                src={project.src}
-                alt={project.alt}
-                width="90"
-                height="90"
-              />
-              <div className="project__details">
-                <h4 className="project__title">{project.title}</h4>
-                <p className="project__description">{project.description}</p>
-                <div className="project__stack">
-                  {project.stack.map((stack) => {
-                    return (
-                      <span key={stack} className="project__tech">
-                        {stack}
-                      </span>
-                    );
-                  })}
+              <div className="project_description_container">
+                <Image
+                  className="project__image"
+                  src={project.src}
+                  alt={project.alt}
+                  width="90"
+                  height="90"
+                />
+                <div className="project__details">
+                  <Link
+                    className="project__title"
+                    href={project.link}
+                    target="_blank"
+                  >
+                    <h4>{project.title}</h4>
+                  </Link>
+                  <p className="project__description">{project.description}</p>
+                  <div className="project__stack">
+                    {project.stack.map((stack) => {
+                      return (
+                        <span key={stack} className="project__tech">
+                          {stack}
+                        </span>
+                      );
+                    })}
+                  </div>
                 </div>
+              </div>
+
+              <div className="project__images">
+                {project.images.map((image, index) => {
+                  return (
+                    <Image
+                      key={index}
+                      className="project__image"
+                      src={image}
+                      alt={project.alt}
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                    />
+                  );
+                })}
               </div>
             </div>
           );
--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,7 +4,14 @@ export const projects = [
     src: "/images/my-finances.png",
     alt: "my-finances-logo",
     title: "Finance Tracker",
+    link: "https://myfinances.vercel.app/",
     description: "A personal finance app that helps you manage your money.",
     stack: ["Next.js", "Tailwind", "TypeScript", "Supabase"],
+    images: [
+      "/images/my-finances/1.png",
+      "/images/my-finances/2.png",
+      "/images/my-finances/3.png",
+      "/images/my-finances/4.png",
+    ],
   },
 ];
